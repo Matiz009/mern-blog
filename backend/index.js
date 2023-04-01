e61115c8309e31d384086141e44046af65e9fe15
MATI UL REHMAN <iammatiz00@gmail.com>
2023-04-02 03:03:23 +0500

    backend completed

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
 const authRoute = require("./routes/auth");
 const authUser = require("./routes/user");
 const authPost = require("./routes/posts");
+const authCat = require("./routes/categories");
+
 app.use(express.json());
+app.use("./images", express.static(path.join(__dirname, "/images")));
 mongoose.set("strictQuery", false);
 mongoose
   .connect("mongodb://127.0.0.1:27017/blog", {
@@ -18,9 +23,24 @@ mongoose
     console.log(error);
   });
 
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    callback(null, "file.png");
+    //callb(null, req.body.name);
+  },
+});
+const upload = multer({ storage: storage });
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
 app.use("/auth", authRoute);
 app.use("/users", authUser);
 app.use("/posts", authPost);
+app.use("/category", authCat);
 app.listen("5000", () => {
     console.log("backend running");
 });
